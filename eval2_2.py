6039a24370373eda0d1db11d0de0edb4bbdedec0
Tanya Gupta <gupta.tanya@gmail.com>
2019-07-20 16:12:51 -0400

    feat: added hypo_explode

--- eval2_2.py
+++ eval2_2.py
@@ -9,66 +9,92 @@ from nltk.corpus import wordnet as wn
 
 from nltk.corpus import stopwords as sw
 
-
-
 def main ():
         corpus_root = '/Users/abirqasem/nlp/dict'
         words = PlaintextCorpusReader(corpus_root, 'sentence.txt').words()
         return words
 
+def create_keys(translator):
+    result = {}
+    for item in translator:
+        result[item] = len(translator[item])
+    return result
+
+def hypo_explode (sent,translator):
+    sents = ""
+    result = []
+    sent_result = sent
+
+    trans_key = create_keys(translator)
+    sent = sent.split(" ")
+    result.append(sent_result)
+
+    for item in sent:
+        if len(translator[item]) != 0:
+            count = 0
+            while count < len(translator[item]):
+
+                new_val = sent_result.replace(item,str(translator[item][count]))
+                result.append(new_val)
+
+                for key in trans_key.keys():
+
+                    if key != item and trans_key[key] > 0:
+                        for i in range(0,len(translator[key])):
+                            result.append(new_val.replace(key,translator[key][i]))
+
+                if len(translator[sent[(sent.index(item))]]) > 0:
+
+                    result.append(new_val.replace(sent[(sent.index(item))],translator[sent[(sent.index(item))]][0]))
+
+                count = count+1
+
+
+
+    print(set(result))
+    return sents
 
 
 def analyze (words):
-    #words = ["jump","dog"]
+
     positions = {}
     clean_words = words
     result = {}
 
-    #stopwords = sw.words('english')
-    #stopwords.append("the")
-
-
     for word in words:
         positions[word] = words.index(word)
-
-        #if (not word.lower() in stopwords):
-        #clean_words.append(word)
-
-
 
     for word in clean_words:
         content = wn.synsets(word,pos="n") #n stands for noun, and a for adjective
         if len(content) > 0:
 
             hypos = content[0].hyponyms()
-            # if len(content) > 0:
-            #     hypos = content[0].hyponyms()
-            #     if len(hypos) != 0:
-            #
+
             temp = []
             for hypo in hypos:
                 temp.append(hypo.lemmas()[0].name())
             result[word] = temp
 
-                # else:
-                #     content = wn.synset(word+".a.01")
-                #     hypos = content.hyponyms()
-                #     if len(hypos) != 0:
-                #         for word in hypos:
-                #             print(word.lemmas()[0].name())
-                #             print("adjective")
         else:
             result[word] = []
-    #print(positions)
-    #print(clean_words)
-    print(result)
 
-    #return len(content.hyponyms())
+    print(result)
 
 
 
 
 if __name__ == '__main__':
 
-    words = main()
-    analyze(words)
+    #words = main()
+    #analyze(words)
+    sent = "a b c a"
+    sent1 = "Assign the Variable to a constant"
+
+
+    translator1 = {"Assign": ["Set value"], "the":[], "to":[], "a":[], "Variable":["memory location", "letter"], "constant": ["number", "fixed number"]}
+
+
+
+    translator = {"a": ["x","y"], "b": [], "c": ["z"]}
+    #print(create_keys(translator))
+    hypo_explode (sent1,translator1)
