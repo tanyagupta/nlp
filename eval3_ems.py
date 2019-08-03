3a246eed8d5299923469c52784df270c85d94458
Tanya Gupta <gupta.tanya@gmail.com>
2019-08-03 09:39:43 -0400

    feat: renamed variables appropriately

--- eval3_ems.py
+++ eval3_ems.py
@@ -6,13 +6,13 @@ from email.message import EmailMessage
 
 def read_em():
     with open("dict/em.txt", 'rb') as fp:
-        headers = BytesParser(policy=default).parse(fp,headersonly=False)
-        print('To: {}'.format(headers['to']))
-        print('From: {}'.format(headers['from']))
-        print('Subject: {}'.format(headers['subject']))
-        print('Recipient username: {}'.format(headers['to'].addresses[0].username))
-        print('Sender name: {}'.format(headers['from'].addresses[0].display_name))
-        print('Body: {}'.format(headers.get_body(preferencelist=('related', 'html', 'plain')).get_content()))
+        content = BytesParser(policy=default).parse(fp,headersonly=False)
+        print('To: {}'.format(content['to']))
+        print('From: {}'.format(content['from']))
+        print('Subject: {}'.format(content['subject']))
+        print('Recipient username: {}'.format(content['to'].addresses[0].username))
+        print('Sender name: {}'.format(content['from'].addresses[0].display_name))
+        print('Body: {}'.format(content.get_body(preferencelist=('related', 'html', 'plain')).get_content()))
         # Got the hint on accessing the body without headers using get_body from https://stackoverflow.com/questions/21449085/python-3-4-email-contentmanager-how-to-use @Brandon Rhodes
 
 if __name__ == '__main__':
